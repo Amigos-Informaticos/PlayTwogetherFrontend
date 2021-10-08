8c84841b7fa951c3559233048180c50b729dbe20
Efrain Arenas <efrain_escuela@outlook.com>
2021-10-07 20:33:28 -0500

    Sign up con JS

--- js/obtener.js
+++ js/obtener.js
@@ -1,28 +1,40 @@
-let $btnRegistrarse = document.getElementById("btnRegistrarse");
+let $btnRegistrarse = document.getElementById("btnSignUp");
 let $tfEmail = document.getElementById("tfEmail");
 let $tfNickname = document.getElementById("tfNickname");
-let $tfContrasenia = document.getElementById("tfContrasenia");
+let $tfContrasenia = document.getElementById("tfPassword");
+let $tfRepetirContrasenia = document.getElementById("tfRepeatPassword");
+let $cbSex = document.getElementById("cbSex");
+let $dpBirthday = document.getElementById("dpBirthday");
+
 
 $btnRegistrarse.addEventListener("click", ()=> {
+    let contrasenia;
+    if ($tfContrasenia.value == $tfRepetirContrasenia.value){
+        contrasenia = $tfContrasenia.value;
+    }else{
+        contrasenia = null
+    }
+
     let nuevoPlayer = {
         email: $tfEmail.value,
         nickname: $tfNickname.value,
-        password: $tfContrasenia.value
+        password: contrasenia,
+        gender: $cbSex.value,
+        birthday: $dpBirthday.value
     }
+
     let opcionesEnvio = {
         method: "POST",
         body: JSON.stringify(nuevoPlayer)
     }
 
+    console.log(opcionesEnvio.body);
+
+
     fetch("http://127.0.0.1:5000/" + "players", opcionesEnvio).then(respuestaSolicitud => {
         console.log(opcionesEnvio.body);
         respuestaSolicitud.json().then(respuestaJson => {
-            let $parrafo = document.getElementById("Parrafo");
-            if (respuestaJson.status == "success"){
-                $parrafo.innerText = "Guardado!";
-            }else {
-                $parrafo.innerText = "No Guardado!";
-            }
+            let $tiulo = document.getElementById("title").innerHTML = respuestaJson;
         })
     })
 })
