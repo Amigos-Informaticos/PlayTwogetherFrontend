05bef8de106a05d959cf939aa02be37265e84879
Efrain Arenas <efrain_escuela@outlook.com>
2021-10-08 00:09:39 -0500

    Login WIP

--- js/obtener.js
+++ js/obtener.js
@@ -5,40 +5,36 @@ let $tfContrasenia = document.getElementById("tfPassword");
 let $tfRepetirContrasenia = document.getElementById("tfRepeatPassword");
 let $cbSex = document.getElementById("cbSex");
 let $dpBirthday = document.getElementById("dpBirthday");
+let $pWarning = document.getElementById("warning");
 
 
 $btnRegistrarse.addEventListener("click", ()=> {
     let contrasenia;
     if ($tfContrasenia.value == $tfRepetirContrasenia.value){
         contrasenia = $tfContrasenia.value;
-    }else{
-        contrasenia = null
-    }
-
-    let nuevoPlayer = {
-        email: $tfEmail.value,
-        nickname: $tfNickname.value,
-        password: contrasenia,
-        gender: $cbSex.value,
-        birthday: $dpBirthday.value
-    }
-
-    let opcionesEnvio = {
-        method: "POST",
-        body: JSON.stringify(nuevoPlayer),
-        headers:{
-            'Content-Type': 'application/json'
+        let nuevoPlayer = {
+            email: $tfEmail.value,
+            nickname: $tfNickname.value,
+            password: contrasenia,
+            gender: $cbSex.value,
+            birthday: $dpBirthday.value
         }
-    }
-
-    console.log(opcionesEnvio.body);
-
-
-    fetch("http://127.0.0.1:5000/" + "players", opcionesEnvio).then(respuestaSolicitud => {
+        let opcionesEnvio = {
+            method: "POST",
+            body: JSON.stringify(nuevoPlayer),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }
         console.log(opcionesEnvio.body);
-        respuestaSolicitud.json().then(respuestaJson => {
-            let $tiulo = document.getElementById("title").innerHTML = respuestaJson;
+        fetch("http://127.0.0.1:5000/" + "players", opcionesEnvio).then(respuestaSolicitud => {
+            respuestaSolicitud.json().then(respuestaJson => {
+                let $tiulo = document.getElementById("title").innerHTML = respuestaJson;
+            })
         })
-    })
+
+    }else{
+        $pWarning.innerHTML = "Las contraseñas no coinciden"
+    }
 })
 
