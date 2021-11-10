06fead6d70f58152e92b17125bda41b6ec65f5c2
Efrain Arenas <efrain_escuela@outlook.com>
2021-11-10 02:52:07 -0600

    Verificar y banear, terminados

--- js/ViewProfile.js
+++ js/ViewProfile.js
@@ -9,63 +9,70 @@ let $btnAddGame = document.getElementById("btnAddGame");
 let $btnConfirmReport = document.getElementById("btnConfirmReport");
 let $btnConfirmVerify = document.getElementById("btnConfirmVerify");
 let $btnConfirmBan = document.getElementById("btnConfirmBan");
+let $verified = document.getElementById("verified");
 
 let profileToShow = sessionStorage.getItem('viewProfile');
 let nickname;
 let birthday;
 let gender;
+let isVerified;
 let isModerator = sessionStorage.getItem('isModerator');
 
-if (profileToShow === "MyProfile"){
+if (profileToShow === "MyProfile") {
     nickname = sessionStorage.getItem('nickname');
     gender = sessionStorage.getItem('gender');
     birthday = sessionStorage.getItem('birthday');
-    showInfo(nickname, birthday, gender);
+    showInfo(nickname, birthday, gender, 0);
 
     $btnReport.remove();
     $btnVerify.remove();
     $btnBan.remove();
 
-}else{
+} else {
     let sendOptions = {
         method: "GET",
     }
     fetch("http://127.0.0.1:5000/players/" + profileToShow, sendOptions).then(response => {
         console.log(response);
-        if (response.ok){
+        if (response.ok) {
             response.json().then(responseJson => {
                 gender = responseJson.gender;
                 birthday = responseJson.birthday;
-                showInfo(profileToShow, birthday, gender);
+                isVerified = responseJson.isVerified;
+                showInfo(profileToShow, birthday, gender, isVerified);
             })
         }
     })
     $btnAddGame.remove();
     $btnEdit.remove();
     console.log("MOD: " + isModerator);
-    if (isModerator != 1){
+    if (isModerator != 1) {
         console.log("NO ADMIN");
         $btnVerify.remove();
         $btnBan.remove();
     }
 }
 
-console.log("Genero "+gender);
-console.log("Fecha "+birthday);
+console.log("Genero " + gender);
+console.log("Fecha " + birthday);
 
-function showInfo(nickname, birthday, gender){
+function showInfo(nickname, birthday, gender, isVerified) {
     $lblNickname.innerText = nickname;
     $lblAge.innerText = getAge(birthday) + " años";
     var playerGender = gender;
     var ScreenGender;
-    if (playerGender == 'F'){
+    if (playerGender == 'F') {
         ScreenGender = "Mujer";
-    }else if (playerGender == 'M'){
+    } else if (playerGender == 'M') {
         ScreenGender = "Hombre";
-    }else {
+    } else {
         ScreenGender = "Compañere";
     }
     $lblGender.innerHTML = ScreenGender;
+
+    if (isVerified != 1) {
+        $verified.remove();
+    }
 }
 
 function getAge(dateString) {
@@ -79,12 +86,12 @@ function getAge(dateString) {
     return age;
 }
 
-$btnEdit.addEventListener("click",(event) =>{
+$btnEdit.addEventListener("click", (event) => {
     event.preventDefault();
     location.href = '../view/UpdateProfile.html'
 })
 
-$btnConfirmReport.addEventListener("click",(event) =>{
+$btnConfirmReport.addEventListener("click", (event) => {
     event.preventDefault();
     let reportInformation = {
         email: $tfEmail.value,
@@ -93,50 +100,57 @@ $btnConfirmReport.addEventListener("click",(event) =>{
     let sendOptions = {
         method: "POST",
         body: JSON.stringify(reportInformation),
-        headers:{
+        headers: {
             'Content-Type': 'application/json'
         }
     }
     fetch("http://127.0.0.1:5000/" + "login", sendOptions).then(response => {
         console.log(response);
-        if (response.ok){
+        if (response.ok) {
             response.json().then(responseJson => {
                 console.log(responseJson);
-                location.href = 'view/Welcome.html';
+                location.href = '../view/Welcome.html';
             })
         }
     })
 })
 
-$btnConfirmVerify.addEventListener("click",(event) =>{
+$btnConfirmVerify.addEventListener("click", (event) => {
     event.preventDefault();
-    let reportInformation = {
-        nickname: profileToShow
-    }
     let sendOptions = {
         method: "PATCH",
-        body: JSON.stringify(reportInformation),
-        headers:{
+        headers: {
             'Content-Type': 'application/json',
             'token': sessionStorage.getItem('token')
         }
     }
-    fetch("http://127.0.0.1:5000/players/" + "verify", sendOptions).then(response => {
+    fetch("http://127.0.0.1:5000/players/" + profileToShow + "/verify", sendOptions).then(response => {
         console.log(response);
-        if (response.ok){
-            response.json().then(responseJson => {
-                console.log(responseJson);
-                location.href = 'view/Welcome.html';
-            })
+        if (response.ok) {
+            location.href = '../view/Welcome.html';
+        }
+    })
+})
+
+$btnConfirmBan.addEventListener("click", (event) => {
+    event.preventDefault();
+    let sendOptions = {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'token': sessionStorage.getItem('token')
+        }
+    }
+    fetch("http://127.0.0.1:5000/players/" + profileToShow + "/ban", sendOptions).then(response => {
+        console.log(response);
+        if (response.ok) {
+            location.href = '../view/Welcome.html';
         }
     })
 })
 
 
-
-
-
-$btnAddGame.addEventListener("click",(event) =>{
+$btnAddGame.addEventListener("click", (event) => {
     event.preventDefault();
     location.href = '../view/AddValorant.html'
 })
@@ -183,8 +197,8 @@ const response = [
 let $template = document.getElementById("template-box").content;
 response.forEach((persona) => {
     $template.querySelector(".card-level").textContent = "LVL:" + persona.level;
-    $template.querySelector(".card-game").src = "../img/"+ persona.game +"/" + persona.game + "_logo.png";
-    $template.querySelector(".card-rank").src = "../img/"+ persona.game +"/rank/" + persona.rank + ".png";
+    $template.querySelector(".card-game").src = "../img/" + persona.game + "/" + persona.game + "_logo.png";
+    $template.querySelector(".card-rank").src = "../img/" + persona.game + "/rank/" + persona.rank + ".png";
 
     let $clone = document.importNode($template, true);
     let $fragment = document.getElementById("game-container");
