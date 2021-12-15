2659fd390895c7da2c46d2d68688ff5214d93388
Efrain Arenas <efrain_escuela@outlook.com>
2021-12-14 22:42:31 -0600

    Puliendo la configuración

--- js/Configuration.js
+++ js/Configuration.js
@@ -1,20 +1,5 @@
 export class Configuration {
-
-
     static getURL() {
-        //return "http://127.0.0.1:5000/";
         return "http://playtwogether.ddns.net:42081/";
     }
-
-    /*static  getURL(){
-        let url = "";
-        $.getJSON("test.json", function(json) {
-            console.log(json); // this will show the info it in firebug console
-        });
-
-        return url;
-    }*/
-
-
 }
-
